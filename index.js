fbbeaf5504032a04ab74fec609f8675d33b712cc
Aznark <polit82@tutanota.com>
2023-02-09 18:31:48 +0000

    Standard javascript style

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function set(name, value, attrs) {
-  document.cookie = name+"="+encodeURIComponent(value)+
-    (attrs ?
-      (attrs.path ? ";path="+attrs.path : "") +
-      (attrs.domain ? ";domain="+attrs.domain : "") +
-      (attrs.maxAge ? ";max-age="+attrs.maxAge : "") +
-      (attrs.expires ? ";expires="+attrs.expires : "") +
-      (attrs.secure ? ";secure" : "") +
-      (attrs.samesite ? ";samesite" : "")
-    : "");
+function set (name, value, attrs) {
+  document.cookie = name + '=' + encodeURIComponent(value) +
+    (attrs
+      ? (attrs.path ? ';path=' + attrs.path : '') +
+      (attrs.domain ? ';domain=' + attrs.domain : '') +
+      (attrs.maxAge ? ';max-age=' + attrs.maxAge : '') +
+      (attrs.expires ? ';expires=' + attrs.expires : '') +
+      (attrs.secure ? ';secure' : '') +
+      (attrs.samesite ? ';samesite' : '')
+      : '')
 }
 
-function get(name) {
-  const cookie = document.cookie.split('; ').find((cookie) => cookie.startsWith(name));
-  return cookie ? decodeURIComponent(cookie.substring(cookie.indexOf("=")+1)) : null;
+function get (name) {
+  const cookie = document.cookie.split('; ').find((cookie) => cookie.startsWith(name))
+  return cookie ? decodeURIComponent(cookie.substring(cookie.indexOf('=') + 1)) : null
 }
 
-function remove(name) {
-  set(name, "", {expires: "0"});
+function remove (name) {
+  set(name, '', { expires: '0' })
 }
 
-module.exports = { set: set, get: get, remove: remove }
+module.exports = { set, get, remove }
